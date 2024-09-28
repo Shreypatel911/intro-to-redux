c08125e36814cd077602e421c5f67cff6346e041
Shreypatel911 <77115893+Shreypatel911@users.noreply.github.com>
2024-09-28 16:46:28 -0700

    complete todo list with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        Intro to Redux
+        <AddTodo/>
+        <Todos/>
       </div>
     </>
   )
